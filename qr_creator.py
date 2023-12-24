f9ead0c93db7a35f93dd25bd893a62872cce4257
Batuhan KURT <134990720+Batuhan1KURT@users.noreply.github.com>
2023-12-24 06:15:45 +0300

    Add files via upload

--- qr_creator.py
+++ qr_creator.py
@@ -1,14 +1,28 @@
 # Import the qrcode library
 import qrcode
 
+# Install  | pip install qrcode[pil] |  library to change the colors etc.
+
 # Get the URL from user input
 url_input = input("Enter the link to create QR code: ")
 
-# Generate the QR code image
-qr_img = qrcode.make(url_input)
+# Create a QRCode instance with customized options
+qrc = qrcode.QRCode(
+    version=2,  # Adjust the QR code version (controls the size)
+    error_correction=qrcode.ERROR_CORRECT_L,  # Error correction level
+    box_size=5,  # Size of each box in the QR code
+    border=2  # Width of the border around the QR code
+)
 
-# Save the QR code image as "git_qr.jpg"
-qr_img.save("git_qr.jpg")
+# Add the data (URL) to the QR code
+qrc.add_data(url_input)
+
+# Generate the QR code image with custom colors
+qr_img = qrc.make_image(fill_color="red", back_color="black")
+
+# Save the QR code image as "git_qr.png"
+qr_img.save("git_qr.png")
 
 # Print a message to indicate successful QR code creation and saving
-print("QR code generated and saved as git_qr.jpg")
+print("QR code generated and saved as git_qr.png")
+
